3d771fcfaa2b5d47ecafadb363c9402cecc0dbb9
admink <admink>
2017-05-31 16:38:46 +0300

    --some fixed

--- landing/views.py
+++ landing/views.py
@@ -6,4 +6,12 @@ def landing(request):
     name = "Jackal"
     current_day = "30.05.17"
     form = SubscribersFrom(request.POST or None)
+
+    if request.method =="POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
     return render(request, 'landing/landing.html', locals())
